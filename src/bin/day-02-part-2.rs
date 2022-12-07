db83939e1506f0d19176fc2981a670e03383fbd0
Balint Soos <soosbalint95@gmail.com>
2022-12-08 00:14:35 +0100

    solve day 02 part 2

--- src/bin/day-02-part-2.rs
+++ src/bin/day-02-part-2.rs
@@ -8,15 +8,15 @@ fn read_lines() -> io::Result<io::Lines<io::BufReader<File>>> {
 
 fn get_point(opponent_pick: &str, your_pick: &str) -> u16 {
     match (opponent_pick, your_pick) {
-        ("A", "X") => 1 + 3,
-        ("A", "Y") => 2 + 6,
-        ("A", "Z") => 3 + 0,
+        ("A", "X") => 3 + 0,
+        ("A", "Y") => 1 + 3,
+        ("A", "Z") => 2 + 6,
         ("B", "X") => 1 + 0,
         ("B", "Y") => 2 + 3,
         ("B", "Z") => 3 + 6,
-        ("C", "X") => 1 + 6,
-        ("C", "Y") => 2 + 0,
-        ("C", "Z") => 3 + 3,
+        ("C", "X") => 2 + 0,
+        ("C", "Y") => 3 + 3,
+        ("C", "Z") => 1 + 6,
         (&_, _) => todo!()
     }
 }
