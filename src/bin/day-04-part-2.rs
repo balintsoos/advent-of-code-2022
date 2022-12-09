55d6cfa7a0c0a66f954eb8851dda6a28bfd1b5f2
Balint Soos <soosbalint95@gmail.com>
2022-12-09 13:13:22 +0100

    solve day 04 part 2

---
+++ src/bin/day-04-part-2.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() {
+    let mut result: u16 = 0;
+
+    if let Ok(lines) = read_lines() {
+        for line in lines {
+            if let Ok(chars) = line {
+                let vec: Vec<&str> = chars.split(",").collect();
+                let vec1: Vec<&str> = vec[0].split("-").collect();
+                let vec2: Vec<&str> = vec[1].split("-").collect();
+                if is_overlap(vec1, vec2) {
+                    result += 1;
+                }
+            }
+        }
+    }
+
+    println!("Result: {}", result);
+}
+
+fn read_lines() -> io::Result<io::Lines<io::BufReader<File>>> {
+    let file = File::open("input.txt")?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn is_overlap(a: Vec<&str>, b: Vec<&str>) -> bool {
+    to_u8(a[1]) >= to_u8(b[0]) && to_u8(a[0]) <= to_u8(b[1])
+}
+
+fn to_u8(s: &str) -> u8 {
+    s.parse::<u8>().unwrap()
+}
