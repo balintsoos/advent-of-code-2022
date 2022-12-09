aaa595b9cc15d7081439b7a2c434639293b071ba
Balint Soos <soosbalint95@gmail.com>
2022-12-09 12:46:03 +0100

    solve day 04 part 1

---
+++ src/bin/day-04-part-1.rs
@@ -0,0 +1,33 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn read_lines() -> io::Result<io::Lines<io::BufReader<File>>> {
+    let file = File::open("input.txt")?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn is_fully_contain(assignment1: Vec<&str>, assignment2: Vec<&str>) -> bool {
+    assignment1[0].parse::<u8>().unwrap() <= assignment2[0].parse::<u8>().unwrap()
+        && assignment1[1].parse::<u8>().unwrap() >= assignment2[1].parse::<u8>().unwrap()
+        || assignment1[0].parse::<u8>().unwrap() >= assignment2[0].parse::<u8>().unwrap()
+            && assignment1[1].parse::<u8>().unwrap() <= assignment2[1].parse::<u8>().unwrap()
+}
+
+fn main() {
+    let mut result: u16 = 0;
+
+    if let Ok(lines) = read_lines() {
+        for line in lines {
+            if let Ok(chars) = line {
+                let vec: Vec<&str> = chars.split(",").collect();
+                let vec1: Vec<&str> = vec[0].split("-").collect();
+                let vec2: Vec<&str> = vec[1].split("-").collect();
+                if is_fully_contain(vec1, vec2) {
+                    result += 1;
+                }
+            }
+        }
+    }
+
+    println!("Result: {}", result);
+}
